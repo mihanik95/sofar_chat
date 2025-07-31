06fec3bdbca02285bd74068f46392c1f954d4a09
mihanik95 <mihanik95@gmail.com>
2025-07-30 17:47:48 -0700

    Bump plugin version to 0.0087

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -77,13 +77,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                0.0086
+#define JucePlugin_Version                0.0087
 #endif
 #ifndef  JucePlugin_VersionCode
  #define JucePlugin_VersionCode            0x5600
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "0.0086"
+#define JucePlugin_VersionString          "0.0087"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
@@ -155,7 +155,7 @@
  #define JucePlugin_ARAFactoryID           "com.MichaelAfanasyev.SOFAR.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.MichaelAfanasyev.SOFAR.aradocumentarchive.0.0086"
+#define JucePlugin_ARADocumentArchiveID   "com.MichaelAfanasyev.SOFAR.aradocumentarchive.0.0087"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
